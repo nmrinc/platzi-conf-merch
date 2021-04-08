a6470d446ad3523c6ae66fd90068b8e4952c9e46
Erick Avila <nmr.inc@icloud.com>
2021-04-07 21:03:29 -0500

    Lesson 22. Maquetacion y estilos del Home

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/components/app.scss';
+import '../../styles/components/App.scss';
 
 const App = () => <h1>Hello World!</h1>;
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Layout from '../components/Layout';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Information from '../containers/Information';
@@ -9,16 +10,18 @@ import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route exact path="/checkout/information" component={Information} />
-      <Route exact path="/checkout/payment" component={Payment} />
-      <Route exact path="/checkout/success" component={Success} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Layout>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </Layout>
 );
 
 export default App;
