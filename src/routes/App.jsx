68ec11022e3fbd4cbe1e7038503202f342da97dc
Erick Avila <nmr.inc@icloud.com>
2021-04-08 20:43:52 -0500

    Lesson 28 Creando nuestro primer custom hook

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faShoppingBasket,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 import Layout from '../components/Layout';
 import Home from '../containers/Home';
@@ -17,19 +21,23 @@ import NotFound from '../containers/NotFound';
 
 library.add(faShoppingBasket, faTrashAlt);
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
-);
-
+const App = () => {
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
+};
 export default App;
