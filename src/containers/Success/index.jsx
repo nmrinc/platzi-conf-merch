0dd3bf94e1b4c0c729202f461b400f3aaaad6b3c
Erick Avila <nmr.inc@icloud.com>
2021-04-21 21:48:25 -0500

    Lesson 37. Integración de Google Maps en el mapa de checkout

--- src/containers/Success/index.jsx
+++ src/containers/Success/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+import Map from '../../components/Map';
 import '../../styles/components/Success.scss';
 
-const Success = () => (
-  <div className="Succes">
-    <div className="Success-content">
-      <h2>Nombre, Gracias por tu compra</h2>
-      <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-      <div className="Success-map">Google Maps</div>
+const Success = () => {
+  const {
+    state: { buyer },
+  } = useContext(AppContext);
+
+  return (
+    <div className="Succes">
+      <div className="Success-content">
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+        <div className="Success-map">
+          <Map />
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Success;
