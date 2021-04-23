b016cba8d2447c269ecb264447ed8c5876fc8b21
Erick Avila <nmr.inc@icloud.com>
2021-04-22 21:13:58 -0500

    38. Creando un Custom Hook para Google Maps

--- src/containers/Success/index.jsx
+++ src/containers/Success/index.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../context/AppContext';
 import Map from '../../components/Map';
+import useGoogleAddress from '../../hooks/useGoogleAddress';
 import '../../styles/components/Success.scss';
 
 const Success = () => {
   const {
     state: { buyer },
   } = useContext(AppContext);
+  const location = useGoogleAddress(buyer[0].address);
 
   return (
     <div className="Succes">
@@ -14,7 +16,7 @@ const Success = () => {
         <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
